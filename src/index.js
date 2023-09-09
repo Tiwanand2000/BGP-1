752436a76b641b61effee852f2f4a022bc361f12
ANAND TIWARI <87767605+Tiwanand2000@users.noreply.github.com>
2023-09-09 19:27:03 +0000

    Developed bauc struct

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
  function App(){
-  return <h1>Hello React!</h1>
+  return <div>
+ <Header/>
+    <Menu/>
+    <Footer/>
+    </div>
+ }
+
+ function Pizza(){
+  return <div>
+    <img src="images/spinaci.jpg" alt="Spinachio"/>
+  <h2>Pizza</h2>
+   </div>
+ }
+
+ function Menu(){
+  return <div>
+    <h2>Our Menu</h2>
+    <Pizza/>
+    <Pizza/>
+    <Pizza/>
+    </div>
+ }
+
+ function Header(){
+  return <h1>Fast React Pizza.Co</h1>
+ }
+
+ function Footer(){
+  return <div>
+  <h4>We're currently open {new Date().toLocaleTimeString()} !! Visit soon </h4>
+   </div>
  }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
